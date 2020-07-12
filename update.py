1c6baf474ea22fa65137faa529ce0dd969aec9a0
jedmonson-rin <jedmonson@novetta.com>
2020-07-11 21:08:55 -0400

    adjustment for auto-updates

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ def main():
     time.sleep(10)
     os.system("git pull")
     time.sleep(60)
-    os.system("python3 run_quetta.py")
+    os.system("python3 run_quetta.py std")
 
 if __name__ == "__main__":
     main()
